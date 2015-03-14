4036013b0d89e13166bf8ad54d66672af84c3cac
Robbie Clutton <robert.clutton@gmail.com>
2015-03-14 15:47:55 +0000

    Spring @scheduled becomes Runnable/executor

--- dis-ingest/src/main/java/io/pivotal/dis/ingest/service/job/IngestJob.java
+++ dis-ingest/src/main/java/io/pivotal/dis/ingest/service/job/IngestJob.java
@@ -3,28 +3,23 @@ package io.pivotal.dis.ingest.service.job;
 import io.pivotal.dis.ingest.service.tfl.UrlProvider;
 import io.pivotal.dis.ingest.service.store.FileStore;
 import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.InputStream;
 
-@Service
-public class IngestJob {
+public class IngestJob implements Runnable {
 
     private final UrlProvider urlProvider;
     private final FileStore fileStore;
 
-    @Autowired
     public IngestJob(UrlProvider urlProvider, FileStore fileStore) {
         this.urlProvider = urlProvider;
         this.fileStore = fileStore;
     }
 
-    @Scheduled(fixedRate = 60_000)
-    public void execute() {
-        try (InputStream is = urlProvider.getUrl().openConnection().getInputStream()) {
+    public void run() {
+        try {
+            InputStream is = urlProvider.getUrl().openConnection().getInputStream();
             fileStore.save(IOUtils.toString(is));
         } catch (IOException e) {
             e.printStackTrace();
