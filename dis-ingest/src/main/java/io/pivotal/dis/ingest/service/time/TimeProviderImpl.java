b08cd8f347a61968d67769dafe55c3951e2bddaa
Robbie Clutton <robert.clutton@gmail.com>
2015-03-14 15:48:40 +0000

    Spring dependency injection becomes POJO DI

--- dis-ingest/src/main/java/io/pivotal/dis/ingest/service/time/TimeProviderImpl.java
+++ dis-ingest/src/main/java/io/pivotal/dis/ingest/service/time/TimeProviderImpl.java
@@ -1,10 +1,7 @@
 package io.pivotal.dis.ingest.service.time;
 
-import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 
-@Service
 public class TimeProviderImpl implements TimeProvider {
     @Override
     public LocalDateTime currentTime() {
