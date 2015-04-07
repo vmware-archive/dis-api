96ecef6b4b6cb722e11a92cb86d1aa7b74f41785
Nick Street <nstreet@pivotal.io>
2015-04-07 15:00:03 +0100

    Extract parsing from Tfl to Dis style data into a class
    Signed-off-by: Tom Anderson <toanderson@pivotal.io>

--- dis-server/src/main/java/io/pivotal/dis/controller/TflProxyController.java
+++ dis-server/src/main/java/io/pivotal/dis/controller/TflProxyController.java
@@ -1,12 +1,8 @@
 package io.pivotal.dis.controller;
 
 import io.pivotal.dis.service.DisruptedLinesService;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -19,28 +15,7 @@ public class TflProxyController {
     }
 
     public Map<String, List<Map<String, String>>> lineDisruptions() throws IOException {
-        return convertJsonArrayToList(disruptedLinesService.getDisruptedLinesJson());
-    }
-
-    private Map<String, List<Map<String, String>>> convertJsonArrayToList(JSONArray lines) {
-        List<Map<String, String>> disruptedLines = new ArrayList<>();
-
-        for (int i = 0; i < lines.length(); i++) {
-            JSONObject line = lines.getJSONObject(i);
-            JSONObject lineStatus = line.getJSONArray("lineStatuses").getJSONObject(0);
-
-            String statusSeverityDescription = lineStatus.getString("statusSeverityDescription");
-            if (!statusSeverityDescription.equals("Good Service")) {
-                HashMap<String, String> disruptedLine = new HashMap<>();
-                disruptedLine.put("line", line.getString("name"));
-                disruptedLine.put("status", statusSeverityDescription);
-                disruptedLines.add(disruptedLine);
-            }
-        }
-
-        Map<String, List<Map<String, String>>> response = new HashMap<>();
-        response.put("disruptions", disruptedLines);
-        return response;
+        return TflToDisTranslator.convertJsonArrayToList(disruptedLinesService.getDisruptedLinesJson());
     }
 
 }
