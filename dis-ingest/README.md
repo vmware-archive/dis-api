7cc93dfac54aa16c32ca3bc1ea664fd0cc6fee40
Tom Anderson <toanderson@pivotal.io>
2015-04-08 12:24:15 +0100

    Get AWS credentials from environment variables rather than a properties file on the classpath
    Signed-off-by: Tom Anderson <toanderson@pivotal.io>

--- dis-ingest/README.md
+++ dis-ingest/README.md
@@ -6,3 +6,19 @@ The following command produces a zip file which Cloud Foundry can accept.
 
     `gradle distZip`
 
+## Deploy
+
+The following variables must be defined in the application's environment:
+
+Variable  | Value
+--------- | -----
+AWS_ACCESS_KEY_ID | Your AWS access key
+AWS_SECRET_KEY | Your AWS secret key
+
+To run locally, create ``gradle.properties``, based on ``gradle.template.properties``, and then use:
+
+```
+gradle run
+```
+
+This will set the necessary variables.
--- dis-ingest/src/main/java/io/pivotal/dis/ingest/config/ApplicationConfig.java
+++ dis-ingest/src/main/java/io/pivotal/dis/ingest/config/ApplicationConfig.java
@@ -1,6 +1,6 @@
 package io.pivotal.dis.ingest.config;
 
-import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.Bucket;
@@ -54,7 +54,7 @@ public class ApplicationConfig {
     }
 
     public AmazonS3 amazonS3() {
-        return new AmazonS3Client(new ClasspathPropertiesFileCredentialsProvider());
+        return new AmazonS3Client(new EnvironmentVariableCredentialsProvider());
     }
 
     public static void main(String[] args) throws IOException {
