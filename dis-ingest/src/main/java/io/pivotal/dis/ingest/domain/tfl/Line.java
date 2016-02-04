c34c5bf20da471589bb5eebb11b3e23a53fb9d3d
Callum Stott <cstott@pivotal.io>
2016-02-04 16:04:43 +0000

    Refactored IngesterTest
    Signed-off-by: Raul Avila <ravila@pivotal.io>

--- dis-ingest/src/main/java/io/pivotal/dis/ingest/domain/tfl/Line.java
+++ dis-ingest/src/main/java/io/pivotal/dis/ingest/domain/tfl/Line.java
@@ -6,11 +6,36 @@ public class Line {
     private String name;
     private List<LineStatus> lineStatuses;
 
+    public Line(String name, List<LineStatus> lineStatuses) {
+        this.name = name;
+        this.lineStatuses = lineStatuses;
+    }
+
     public List<LineStatus> getLineStatuses() {
         return lineStatuses;
     }
 
     public String getName() {
         return name;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Line line = (Line) o;
+
+        if (name != null ? !name.equals(line.name) : line.name != null) return false;
+        return !(lineStatuses != null ? !lineStatuses.equals(line.lineStatuses) : line.lineStatuses != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (lineStatuses != null ? lineStatuses.hashCode() : 0);
+        return result;
     }
 }
--- dis-ingest/src/test/java/io/pivotal/dis/ingest/service/IngesterTest.java
+++ dis-ingest/src/test/java/io/pivotal/dis/ingest/service/IngesterTest.java
@@ -1,12 +1,15 @@
 package io.pivotal.dis.ingest.service;
 
-import com.amazonaws.util.json.JSONArray;
 import com.amazonaws.util.json.JSONException;
-import com.amazonaws.util.json.JSONObject;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
 import com.squareup.okhttp.mockwebserver.MockResponse;
 import com.squareup.okhttp.mockwebserver.MockWebServer;
-
 import io.pivotal.dis.ingest.config.OngoingDisruptionsStore;
+import io.pivotal.dis.ingest.domain.Digest;
+import io.pivotal.dis.ingest.domain.DisruptedLine;
+import io.pivotal.dis.ingest.domain.tfl.Line;
+import io.pivotal.dis.ingest.domain.tfl.LineStatus;
 import io.pivotal.dis.ingest.service.job.Clock;
 import io.pivotal.dis.ingest.service.job.Ingester;
 import io.pivotal.dis.ingest.service.store.FileStore;
@@ -16,112 +19,198 @@ import org.junit.Test;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
+import static com.squareup.moshi.Types.newParameterizedType;
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.junit.Assert.assertThat;
 
 public class IngesterTest {
 
+    private static final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss");
+    private static final DateTimeFormatter TIME_FORMAT = DateTimeFormatter.ofPattern("HH:mm");
+
     private final MockWebServer tflMockWebServer = new MockWebServer();
     private final MockFileStore rawFileStore = new MockFileStore();
     private final MockFileStore digestedFileStore = new MockFileStore();
-    private OngoingDisruptionsStore ongoingDisruptionsStore;
+    private final OngoingDisruptionsStore ongoingDisruptionsStore = new OngoingDisruptionsStore();
 
     @Before
     public void prepareServer() throws Exception {
-        tflMockWebServer.enqueue(new MockResponse()
+        JsonAdapter<List<Line>> moshiTflLinesAdapter = moshiTflLinesAdapter();
+
+        List<Line> tflLines = new ArrayList<>();
+        tflLines.add(stubLine("Bakerloo", "Runaway Train"));
+
+        tflMockWebServer.enqueue(
+                new MockResponse()
                         .setHeader("Content-Type", "application/json")
-                        .setBody("[{\"name\": \"Bakerloo\", \"lineStatuses\": [{\"statusSeverityDescription\": \"Runaway Train\"}]}]")
-        );
-        tflMockWebServer.enqueue(new MockResponse()
+                        .setBody(moshiTflLinesAdapter.toJson(tflLines)));
+
+        tflLines = new ArrayList<>();
+        tflLines.add(stubLine("Bakerloo", "Runaway Train"));
+        tflLines.add(stubLine("Circle", "Leaves on the Line"));
+
+        tflMockWebServer.enqueue(
+                new MockResponse()
                         .setHeader("Content-Type", "application/json")
-                        .setBody("[" +
-                                "{\"name\": \"Bakerloo\", \"lineStatuses\": [{\"statusSeverityDescription\": \"Runaway Train\"}]}," +
-                                "{\"name\": \"Circle\", \"lineStatuses\": [{\"statusSeverityDescription\": \"Leaves on the Line\"}]}" +
-                                "]")
-        );
+                        .setBody(moshiTflLinesAdapter.toJson(tflLines)));
+
         tflMockWebServer.play();
-        ongoingDisruptionsStore = new OngoingDisruptionsStore();
     }
 
     @Test
     public void savesTflDataToFileStore() throws IOException, JSONException {
-        Clock clock = new FakeClock(LocalDateTime.now());
-        Ingester job = new Ingester(tflMockWebServer.getUrl("/"), rawFileStore, digestedFileStore, clock, ongoingDisruptionsStore);
+        Clock clockNow = new FakeClock(LocalDateTime.now());
+
+        Ingester job = createIngester(clockNow);
         job.ingest();
-        assertThat(rawFileStore.getLastName(), equalTo("tfl_api_line_mode_status_tube_" + clock.getCurrentTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss")) + ".json"));
-        String lastFile = rawFileStore.getLastFile();
-        JSONArray lastFileAsJson = new JSONArray(lastFile);
-        JSONObject line = lastFileAsJson.getJSONObject(0);
-        assertThat(line.get("name"), equalTo("Bakerloo"));
-        JSONArray lineStatuses = (JSONArray) line.get("lineStatuses");
-        JSONObject lineStatus = (JSONObject) lineStatuses.get(0);
-        assertThat(lineStatus.get("statusSeverityDescription"), equalTo("Runaway Train"));
+
+        assertLastRawFileCreated(clockNow);
+
+        List<Line> lines = getLastRawFileContent();
+
+        assertThat(lines.get(0), equalTo(stubLine("Bakerloo", "Runaway Train")));
     }
 
     @Test
     public void savesTflDataToFileStoreForTwoSuccessiveIngestJobs() throws IOException {
-        FakeClock clockNow = new FakeClock(LocalDateTime.now());
-        Ingester job = new Ingester(tflMockWebServer.getUrl("/"), rawFileStore, digestedFileStore, clockNow, ongoingDisruptionsStore);
-        job.ingest();
-        assertThat(rawFileStore.getLastName(), equalTo("tfl_api_line_mode_status_tube_" + clockNow.getCurrentTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss")) + ".json"));
-        assertThat(rawFileStore.getLastFile(), equalTo("[{\"name\": \"Bakerloo\", \"lineStatuses\": [{\"statusSeverityDescription\": \"Runaway Train\"}]}]"));
+        Clock clockNow = new FakeClock(LocalDateTime.now());
 
-        FakeClock clockInTenMinutesTime = new FakeClock(LocalDateTime.now().plusMinutes(10));
-        Ingester secondJob = new Ingester(tflMockWebServer.getUrl("/"), rawFileStore, digestedFileStore, clockInTenMinutesTime, ongoingDisruptionsStore);
+        Ingester job = createIngester(clockNow);
+        job.ingest();
+
+        assertLastRawFileCreated(clockNow);
+
+        List<Line> lines = getLastRawFileContent();
+
+        assertThat(lines.get(0), equalTo(stubLine("Bakerloo", "Runaway Train")));
+
+
+        Clock clockInTenMinutesTime = new FakeClock(LocalDateTime.now().plusMinutes(10));
+
+        Ingester secondJob = createIngester(clockInTenMinutesTime);
         secondJob.ingest();
 
-        assertThat(rawFileStore.getLastName(), equalTo("tfl_api_line_mode_status_tube_" + clockInTenMinutesTime.getCurrentTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss")) + ".json"));
-        assertThat(rawFileStore.getLastFile(), equalTo("[" +
-                                "{\"name\": \"Bakerloo\", \"lineStatuses\": [{\"statusSeverityDescription\": \"Runaway Train\"}]}," +
-                                "{\"name\": \"Circle\", \"lineStatuses\": [{\"statusSeverityDescription\": \"Leaves on the Line\"}]}" +
-                                "]"));
+        assertLastRawFileCreated(clockInTenMinutesTime);
+
+        lines = getLastRawFileContent();
+
+        assertThat(lines.get(0), equalTo(stubLine("Bakerloo", "Runaway Train")));
+        assertThat(lines.get(1), equalTo(stubLine("Circle", "Leaves on the Line")));
     }
 
     @Test
     public void savesTranslatedDataToFileStore() throws Exception {
-        Ingester job = new Ingester(tflMockWebServer.getUrl("/"), rawFileStore, digestedFileStore, new FakeClock(LocalDateTime.now()), ongoingDisruptionsStore);
+        Clock clockNow = new FakeClock(LocalDateTime.now());
+
+        Ingester job = createIngester(clockNow);
         job.ingest();
 
+        assertDigestedFileCreated();
+
+        Digest digest = getLastDigest();
+
+        List<DisruptedLine> disruptions = digest.getDisruptions();
+
+        assertThat(disruptions, hasSize(1));
+
         LocalDateTime currentTime = LocalDateTime.now();
-        assertThat(digestedFileStore.getLastName(), equalTo("disruptions.json"));
-            assertThat(digestedFileStore.getLastFile(), equalTo("{\"disruptions\":" +
-            "[{\"endTime\":\"" + currentTime.format(DateTimeFormatter.ofPattern("HH:mm")) + "\"," +
-            "\"line\":\"Bakerloo\"," +
-            "\"startTime\":\"" + currentTime.format(DateTimeFormatter.ofPattern("HH:mm")) + "\"," +
-            "\"status\":\"Runaway Train\"" +
-            "}]}"
-        ));
+        assertLineData(disruptions.get(0), currentTime, "Bakerloo", "Runaway Train");
     }
 
     @Test
     public void savesTranslatedDataToFileStoreForTwoSuccessiveIngestJobs() throws Exception {
-        LocalDateTime currentTime = LocalDateTime.now();
-        Ingester job = new Ingester(tflMockWebServer.getUrl("/"), rawFileStore, digestedFileStore, new FakeClock(LocalDateTime.now()), ongoingDisruptionsStore);
+        Clock clockNow = new FakeClock(LocalDateTime.now());
+
+        Ingester job = createIngester(clockNow);
         job.ingest();
 
-        Ingester secondJob = new Ingester(tflMockWebServer.getUrl("/"), rawFileStore, digestedFileStore, new FakeClock(LocalDateTime.now().plusMinutes(10)), ongoingDisruptionsStore);
+        Clock clockInTenMinutesTime = new FakeClock(LocalDateTime.now().plusMinutes(10));
+
+        Ingester secondJob = createIngester(clockInTenMinutesTime);
         secondJob.ingest();
 
+        assertDigestedFileCreated();
+
+        Digest digest = getLastDigest();
+
+        List<DisruptedLine> disruptions = digest.getDisruptions();
+
+        assertThat(disruptions, hasSize(2));
+
+        LocalDateTime currentTime = LocalDateTime.now();
+        assertLineData(disruptions.get(0), currentTime, "Bakerloo", "Runaway Train");
+        assertLineData(disruptions.get(1), currentTime.plusMinutes(10), "Circle", "Leaves on the Line");
+    }
+
+
+    private Line stubLine(String name, String...lineStatus) {
+        Line line = new Line(name, stubLineStatuses(lineStatus));
+        return line;
+    }
+
+    private List<LineStatus> stubLineStatuses(String... lineStatus) {
+        return asList(lineStatus).stream()
+                .map(l -> new LineStatus(l))
+                .collect(toList());
+    }
+
+    private void assertLineData(DisruptedLine disruptedLine,
+                                LocalDateTime currentTime,
+                                String expectedLine,
+                                String expectedStatus) {
+
+        assertThat(disruptedLine.getLine(), equalTo(expectedLine));
+        assertThat(disruptedLine.getStartTime(), equalTo(currentTime.format(TIME_FORMAT)));
+        assertThat(disruptedLine.getEndTime(), equalTo(currentTime.format(TIME_FORMAT)));
+        assertThat(disruptedLine.getStatus(), equalTo(expectedStatus));
+    }
+
+    private Ingester createIngester(Clock clock) {
+        return new Ingester(
+                tflMockWebServer.getUrl("/"),
+                rawFileStore,
+                digestedFileStore,
+                clock,
+                ongoingDisruptionsStore);
+    }
+
+    private JsonAdapter<List<Line>> moshiTflLinesAdapter() {
+        return moshi().adapter(newParameterizedType(List.class, Line.class));
+    }
+
+    private JsonAdapter<Digest> moshiDigestAdapter() {
+        return moshi().adapter(Digest.class);
+    }
+
+    private Moshi moshi() {
+        return new Moshi.Builder().build();
+    }
+
+    private void assertLastRawFileCreated(Clock clock) {
+        assertThat(rawFileStore.getLastName(), equalTo("tfl_api_line_mode_status_tube_" + clock.getCurrentTime().format(DATE_TIME_FORMAT) + ".json"));
+    }
+
+    private List<Line> getLastRawFileContent() throws IOException {
+        String lastFileAsJson = rawFileStore.getLastFile();
+        return moshiTflLinesAdapter().fromJson(lastFileAsJson);
+    }
+
+    private void assertDigestedFileCreated() {
         assertThat(digestedFileStore.getLastName(), equalTo("disruptions.json"));
+    }
 
-
-        assertThat(digestedFileStore.getLastFile(), equalTo("{\"disruptions\":[" +
-                "{\"endTime\":\"" + currentTime.format(DateTimeFormatter.ofPattern("HH:mm")) + "\"," +
-                "\"line\":\"Bakerloo\"," +
-                "\"startTime\":\"" + currentTime.format(DateTimeFormatter.ofPattern("HH:mm")) + "\"," +
-                "\"status\":\"Runaway Train\"" +
-                "}," +
-                "{\"endTime\":\"" + currentTime.plusMinutes(10).format(DateTimeFormatter.ofPattern("HH:mm")) + "\"," +
-                "\"line\":\"Circle\"," +
-                "\"startTime\":\"" + currentTime.plusMinutes(10).format(DateTimeFormatter.ofPattern("HH:mm")) + "\"," +
-                "\"status\":\"Leaves on the Line\"" +
-                "}" +
-                "]}"));
+    private Digest getLastDigest() throws IOException {
+        String lastFileAsJson = digestedFileStore.getLastFile();
+        return moshiDigestAdapter().fromJson(lastFileAsJson);
     }
 
     private class MockFileStore implements FileStore {
-
         private String lastName;
         private String lastFile;
 
@@ -141,7 +230,7 @@ public class IngesterTest {
 
     }
 
-    public static class FakeClock implements Clock {
+    private static class FakeClock implements Clock {
         private final LocalDateTime time;
 
         FakeClock(LocalDateTime time) {
