e30d7af56155d0bccf19114536de73f2f8380625
Denisse Zavala <dezavala@pivotal.io>
2016-02-09 17:31:30 +0000

    Remove AlamoFire
    Signed-off-by: Winna Bridgewater <wbridgewater@pivotal.io>

--- dis-ios/dis-ios/DisruptionsService.swift
+++ dis-ios/dis-ios/DisruptionsService.swift
@@ -1,5 +1,4 @@
 import Foundation
-import Alamofire
 import SwiftyJSON
 
 public enum DisruptionsDataKeys : String {
@@ -8,45 +7,33 @@ public enum DisruptionsDataKeys : String {
 }
 
 public class DisruptionsService: DisruptionsServiceProtocol {
-
-    var alamofireManager: Manager
-    
-    init(){
+  
+    init() {
         let config = NSURLSessionConfiguration.defaultSessionConfiguration()
         config.timeoutIntervalForRequest = 10
-        config.HTTPAdditionalHeaders = Manager.defaultHTTPHeaders
-        
-        self.alamofireManager = Alamofire.Manager(configuration: config)
     }
     
-    public func getDisruptions(onSuccess: (disruptions: [String]) -> Void, onError: (error: String) -> Void){
-        var url = ""
-        
+    public func getDisruptions(onSuccess: (disruptions: [String]) -> Void, onError: (error: String) -> Void) {
         #if TEST
-            url = "http://localhost:8080/disruptions.json"
+            let url = NSURL(string: "http://localhost:8080/disruptions.json")!
         #else
-            url = "https://pivotal-london-dis-digest-test.s3.amazonaws.com/disruptions.json"
+            let url = NSURL(string: "https://pivotal-london-dis-digest-test.s3.amazonaws.com/disruptions.json")!
         #endif
         
-        self.alamofireManager.request(.GET, url).validate().responseJSON { response in
-            switch response.result {
-            case .Success:
-                if let value = response.result.value {
-
-                    var disruptions: [String] = []
-                    for disruptionJSON in JSON(value)[DisruptionsDataKeys.Root.rawValue].arrayValue {
-                        if let lineName = disruptionJSON[DisruptionsDataKeys.Line.rawValue].string {
-                            disruptions.append(lineName)
-                        }
-                    }
-                    
-                    onSuccess(disruptions: disruptions)
+        let request = NSURLRequest(URL: url)
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
+            if let data = data {
+                var json = JSON(data: data)
+                let disruptions = json[DisruptionsDataKeys.Root.rawValue].arrayValue.flatMap() { line in
+                    return line[DisruptionsDataKeys.Line.rawValue].string
                 }
-            case .Failure(_):
+
+                onSuccess(disruptions: disruptions)
+            } else {
                 onError(error: "Couldn't retrieve data from server :(")
             }
         }
-        
-        
+        task.resume()
     }
+    
 }
