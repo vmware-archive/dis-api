6685b56d3384e86e488758fb67143aa185b49091
Callum Stott <cs@pivotal.io>
2016-03-08 16:50:50 +0000

    Convert tests to kotlin

---
+++ dis-ingest/src/test/kotlin/io/pivotal/dis/ingest/job/IngestionTest.kt
@@ -0,0 +1,235 @@
+package io.pivotal.dis.ingest.job
+
+import com.amazonaws.util.json.JSONException
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.okhttp.mockwebserver.MockResponse
+import com.squareup.okhttp.mockwebserver.MockWebServer
+import io.pivotal.dis.ingest.domain.Digest
+import io.pivotal.dis.ingest.domain.DisruptedLine
+import io.pivotal.dis.ingest.domain.tfl.TflLine
+import io.pivotal.dis.ingest.domain.tfl.LineStatus
+import io.pivotal.dis.ingest.store.FileStore
+import io.pivotal.dis.ingest.store.OngoingDisruptionsStore
+import io.pivotal.dis.ingest.system.Clock
+import org.junit.Before
+import org.junit.Test
+
+import java.io.IOException
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.ArrayList
+
+import com.squareup.moshi.Types.newParameterizedType
+import java.util.Arrays.asList
+import java.util.stream.Collectors.toList
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.collection.IsCollectionWithSize.hasSize
+import org.junit.Assert.assertThat
+
+class IngestionTest {
+
+    private val tflMockWebServer = MockWebServer()
+    private val rawFileStore = MockFileStore()
+    private val digestedFileStore = MockFileStore()
+    private val ongoingDisruptionsStore = OngoingDisruptionsStore()
+
+    @Before
+    @Throws(Exception::class)
+    fun prepareServer() {
+        val moshiTflLinesAdapter = moshiTflLinesAdapter()
+
+        var tflTflLines: MutableList<TflLine> = ArrayList()
+        tflTflLines.add(stubLine("Bakerloo", "Runaway Train"))
+
+        tflMockWebServer.enqueue(
+                MockResponse().setHeader("Content-Type", "application/json").setBody(moshiTflLinesAdapter.toJson(tflTflLines)))
+
+        tflTflLines = ArrayList<TflLine>()
+        tflTflLines.add(stubLine("Bakerloo", "Runaway Train"))
+        tflTflLines.add(stubLine("Circle", "Leaves on the TflLine"))
+
+        tflMockWebServer.enqueue(
+                MockResponse().setHeader("Content-Type", "application/json").setBody(moshiTflLinesAdapter.toJson(tflTflLines)))
+
+        tflMockWebServer.play()
+    }
+
+    @Test
+    @Throws(IOException::class, JSONException::class)
+    fun savesTflDataToFileStore() {
+        val clock = FakeClock(LocalDateTime.now())
+
+        val job = createIngester()
+        job.ingest(clock)
+
+        assertLastRawFileCreated(clock)
+
+        val tflLines = lastRawFileContent
+
+        assertThat(tflLines[0], equalTo(stubLine("Bakerloo", "Runaway Train")))
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun savesTflDataToFileStoreForTwoSuccessiveIngestJobs() {
+        val clock = FakeClock(LocalDateTime.now())
+
+        val job = createIngester()
+        job.ingest(clock)
+
+        assertLastRawFileCreated(clock)
+
+        var tflLines = lastRawFileContent
+
+        assertThat(tflLines[0], equalTo(stubLine("Bakerloo", "Runaway Train")))
+
+
+        clock.currentTime = LocalDateTime.now().plusMinutes(10)
+        job.ingest(clock)
+
+        assertLastRawFileCreated(clock)
+
+        tflLines = lastRawFileContent
+
+        assertThat(tflLines[0], equalTo(stubLine("Bakerloo", "Runaway Train")))
+        assertThat(tflLines[1], equalTo(stubLine("Circle", "Leaves on the TflLine")))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun savesTranslatedDataToFileStore() {
+        val clock = FakeClock(LocalDateTime.now())
+
+        val job = createIngester()
+        job.ingest(clock)
+
+        assertDigestedFileCreated()
+
+        val digest = lastDigest
+
+        val disruptions = digest.disruptions
+
+        assertThat(disruptions, hasSize<DisruptedLine>(1))
+
+        val currentTime = LocalDateTime.now()
+        assertLineData(disruptions[0], "Bakerloo", "Runaway Train", currentTime, currentTime, currentTime, currentTime)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun savesTranslatedDataToFileStoreForTwoSuccessiveIngestJobs() {
+        val currentTime = LocalDateTime.now()
+
+        val clock = FakeClock(currentTime)
+
+        val job = createIngester()
+        job.ingest(clock)
+
+        clock.currentTime = currentTime.plusMinutes(10)
+        job.ingest(clock)
+
+        assertDigestedFileCreated()
+
+        val digest = lastDigest
+        val disruptions = digest.disruptions
+
+        assertThat(disruptions, hasSize<DisruptedLine>(2))
+
+        assertLineData(disruptions[0], "Bakerloo", "Runaway Train", currentTime, currentTime, currentTime, currentTime)
+        assertLineData(disruptions[1], "Circle", "Leaves on the TflLine", currentTime.plusMinutes(10), currentTime.plusMinutes(10), currentTime.plusMinutes(10), currentTime.plusMinutes(10))
+    }
+
+
+    private fun stubLine(name: String, vararg lineStatus: String): TflLine {
+        val tflLine = TflLine(name, stubLineStatuses(*lineStatus))
+        return tflLine
+    }
+
+    private fun stubLineStatuses(vararg lineStatus: String): List<LineStatus> {
+        return asList(*lineStatus).map({ l -> LineStatus(l) })
+    }
+
+    private fun assertLineData(disruptedLine: DisruptedLine,
+                               expectedLine: String,
+                               expectedStatus: String,
+                               expectedStartTime: LocalDateTime,
+                               expectedEndTime: LocalDateTime,
+                               expectedEarliestEndTime: LocalDateTime,
+                               expectedLatestEndTime: LocalDateTime) {
+
+        assertThat(disruptedLine.line, equalTo(expectedLine))
+        assertThat(disruptedLine.startTime, equalTo(expectedStartTime.format(TIME_FORMAT)))
+        assertThat(disruptedLine.endTime, equalTo(expectedEndTime.format(TIME_FORMAT)))
+        assertThat(disruptedLine.earliestEndTime, equalTo(expectedEarliestEndTime.format(TIME_FORMAT)))
+        assertThat(disruptedLine.latestEndTime, equalTo(expectedLatestEndTime.format(TIME_FORMAT)))
+        assertThat(disruptedLine.status, equalTo(expectedStatus))
+    }
+
+    private fun createIngester(): Ingester {
+        return Ingester(
+                tflMockWebServer.getUrl("/"),
+                rawFileStore,
+                digestedFileStore,
+                ongoingDisruptionsStore)
+    }
+
+    private fun moshiTflLinesAdapter(): JsonAdapter<List<TflLine>> {
+        return moshi().adapter<List<TflLine>>(newParameterizedType(List::class.java, TflLine::class.java))
+    }
+
+    private fun moshiDigestAdapter(): JsonAdapter<Digest> {
+        return moshi().adapter(Digest::class.java)
+    }
+
+    private fun moshi(): Moshi {
+        return Moshi.Builder().build()
+    }
+
+    private fun assertLastRawFileCreated(clock: Clock) {
+        assertThat<String>(rawFileStore.lastName, equalTo("tfl_api_line_mode_status_tube_" + clock.currentTime.format(DATE_TIME_FORMAT) + ".json"))
+    }
+
+    private val lastRawFileContent: List<TflLine>
+        @Throws(IOException::class)
+        get() {
+            val lastFileAsJson = rawFileStore.lastFile
+            return moshiTflLinesAdapter().fromJson(lastFileAsJson)
+        }
+
+    private fun assertDigestedFileCreated() {
+        assertThat<String>(digestedFileStore.lastName, equalTo("disruptions.json"))
+    }
+
+    private val lastDigest: Digest
+        @Throws(IOException::class)
+        get() {
+            val lastFileAsJson = digestedFileStore.lastFile
+            return moshiDigestAdapter().fromJson(lastFileAsJson)
+        }
+
+    private inner class MockFileStore : FileStore {
+        var lastName: String? = null
+            private set
+        var lastFile: String? = null
+            private set
+
+        override fun save(name: String, input: String) {
+            lastName = name
+            lastFile = input
+        }
+
+    }
+
+    private class FakeClock(private var time: LocalDateTime) : Clock {
+
+        override fun getCurrentTime(): LocalDateTime = time
+
+        fun setCurrentTime(newTime: LocalDateTime) { time = newTime }
+    }
+
+    companion object {
+        private val DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss")
+        private val TIME_FORMAT = DateTimeFormatter.ofPattern("HH:mm")
+    }
+}
---
+++ dis-ingest/src/test/kotlin/io/pivotal/dis/ingest/job/TflDigestorTest.kt
@@ -0,0 +1,185 @@
+package io.pivotal.dis.ingest.job
+
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import io.pivotal.dis.ingest.domain.Digest
+import io.pivotal.dis.ingest.domain.DisruptedLine
+import org.apache.commons.io.IOUtils
+import org.junit.Test
+
+import java.io.IOException
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import java.util.Optional
+
+import org.hamcrest.Matchers.equalTo
+import org.junit.Assert.assertThat
+
+class TflDigestorTest {
+
+    @Test
+    @Throws(Exception::class)
+    fun digestTflData_returnsDisruptedLines_WithCorrectDisruptionStartTimes() {
+        val currentTime = LocalDateTime.now()
+
+        val earlyTflLineStatus = loadFixture("line_mode_tube_status")
+        val earlierDigest = TflDigestor(earlyTflLineStatus,
+                currentTime,
+                Optional.empty<String>()).digest()
+
+        val digestsAdapter = digestsAdapter
+
+        var digest = digestsAdapter.fromJson(earlierDigest)
+        var disruptions = digest.disruptions
+
+        assertThat(disruptions.size, equalTo(1))
+
+        var disruptedLine = disruptions[0]
+        assertLineData(disruptedLine, currentTime, "Bakerloo", "Minor Delays", "#FFFFFF", "#AE6118")
+
+        val tenMinutesLater = currentTime.plusMinutes(10)
+        val laterTflLineStatus = loadFixture("line_mode_tube_status_2")
+
+        val laterDigest = TflDigestor(laterTflLineStatus,
+                tenMinutesLater,
+                Optional.of(earlierDigest)).digest()
+
+        digest = digestsAdapter.fromJson(laterDigest)
+        disruptions = digest.disruptions
+
+        assertThat(disruptions.size, equalTo(2))
+
+        disruptedLine = disruptions[0]
+        assertLineData(disruptedLine, currentTime, "Bakerloo", "Minor Delays", "#FFFFFF", "#AE6118")
+
+        disruptedLine = disruptions[1]
+        assertLineData(disruptedLine, tenMinutesLater, "Circle", "Minor Delays", "#113892", "#F8D42D")
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun digestTflData_predictsEndTimeForEachStatus() {
+        val currentTime = LocalDateTime.now()
+
+        val allStatusesJson = loadFixture("endTimeTest")
+
+        val digestJson = TflDigestor(allStatusesJson,
+                currentTime,
+                Optional.empty<String>()).digest()
+
+
+        val digestsAdapter = digestsAdapter
+
+        val digest = digestsAdapter.fromJson(digestJson)
+        val disruptions = digest.disruptions
+
+        assertThat(disruptions.size, equalTo(6))
+
+        assertThat(disruptions[0].endTime, equalTo(currentTime.plusMinutes(30).format(TIME_FORMAT)))
+        assertThat(disruptions[1].endTime, equalTo(currentTime.plusMinutes(60).format(TIME_FORMAT)))
+        assertThat(disruptions[2].endTime, equalTo(currentTime.plusDays(1).format(TIME_FORMAT)))
+        assertThat(disruptions[3].endTime, equalTo(currentTime.plusDays(1).format(TIME_FORMAT)))
+        assertThat(disruptions[4].endTime, equalTo(currentTime.plusMinutes(120).format(TIME_FORMAT)))
+        assertThat(disruptions[5].endTime, equalTo(currentTime.format(TIME_FORMAT)))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun digestTflData_predictsEarliestPossibleEndTimeForEachStatus() {
+        val currentTime = LocalDateTime.now()
+
+        val allStatusesJson = loadFixture("endTimeTest")
+
+        val digestJson = TflDigestor(allStatusesJson,
+                currentTime,
+                Optional.empty<String>()).digest()
+
+        val digestsAdapter = digestsAdapter
+
+        val digest = digestsAdapter.fromJson(digestJson)
+        val disruptions = digest.disruptions
+
+        assertThat(disruptions.size, equalTo(6))
+
+        assertThat(disruptions[0].earliestEndTime, equalTo(currentTime.plusMinutes((30 - 10).toLong()).format(TIME_FORMAT)))
+        assertThat(disruptions[1].earliestEndTime, equalTo(currentTime.plusMinutes((60 - 20).toLong()).format(TIME_FORMAT)))
+        assertThat(disruptions[2].earliestEndTime, equalTo(currentTime.plusHours((24 - 8).toLong()).format(TIME_FORMAT)))
+        assertThat(disruptions[3].earliestEndTime, equalTo(currentTime.plusHours((24 - 8).toLong()).format(TIME_FORMAT)))
+        assertThat(disruptions[4].earliestEndTime, equalTo(currentTime.plusMinutes((120 - 40).toLong()).format(TIME_FORMAT)))
+        assertThat(disruptions[5].earliestEndTime, equalTo(currentTime.format(TIME_FORMAT)))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun digestTflData_predictsLatestPossibleEndTimeForEachStatus() {
+        val currentTime = LocalDateTime.now()
+
+        val allStatusesJson = loadFixture("endTimeTest")
+
+        val digestJson = TflDigestor(allStatusesJson,
+                currentTime,
+                Optional.empty<String>()).digest()
+
+        val digestsAdapter = digestsAdapter
+
+        val digest = digestsAdapter.fromJson(digestJson)
+        val disruptions = digest.disruptions
+
+        assertThat(disruptions.size, equalTo(6))
+
+        assertThat(disruptions[0].latestEndTime, equalTo(currentTime.plusMinutes((30 + 10).toLong()).format(TIME_FORMAT)))
+        assertThat(disruptions[1].latestEndTime, equalTo(currentTime.plusMinutes((60 + 20).toLong()).format(TIME_FORMAT)))
+        assertThat(disruptions[2].latestEndTime, equalTo(currentTime.plusHours((24 + 8).toLong()).format(TIME_FORMAT)))
+        assertThat(disruptions[3].latestEndTime, equalTo(currentTime.plusHours((24 + 8).toLong()).format(TIME_FORMAT)))
+        assertThat(disruptions[4].latestEndTime, equalTo(currentTime.plusMinutes((120 + 40).toLong()).format(TIME_FORMAT)))
+        assertThat(disruptions[5].latestEndTime, equalTo(currentTime.format(TIME_FORMAT)))
+    }
+
+    @Throws(IOException::class)
+    private fun loadFixture(name: String): String {
+        return IOUtils.toString(javaClass.classLoader.getResourceAsStream(name + ".json"))
+    }
+
+    private val digestsAdapter: JsonAdapter<Digest>
+        get() {
+            val moshi = Moshi.Builder().build()
+            return moshi.adapter(Digest::class.java)
+        }
+
+    private fun assertLineData(disruptedLine: DisruptedLine,
+                               currentTime: LocalDateTime,
+                               expectedLine: String,
+                               expectedStatus: String,
+                               expectedForegroundColor: String,
+                               expectedBackgroundColor: String) {
+
+        assertThat(disruptedLine.line, equalTo(expectedLine))
+
+        assertThat(disruptedLine.foregroundColor, equalTo(expectedForegroundColor))
+        assertThat(disruptedLine.backgroundColor, equalTo(expectedBackgroundColor))
+
+        assertThat(disruptedLine.startTime, equalTo(currentTime.format(TIME_FORMAT)))
+        assertThat(disruptedLine.endTime, equalTo(currentTime.plusMinutes(30).format(TIME_FORMAT)))
+        assertThat(disruptedLine.earliestEndTime, equalTo(currentTime.plusMinutes((30 - 10).toLong()).format(TIME_FORMAT)))
+        assertThat(disruptedLine.latestEndTime, equalTo(currentTime.plusMinutes((30 + 10).toLong()).format(TIME_FORMAT)))
+
+        assertThat(epochMillisToTimeString(disruptedLine.startTimestamp), equalTo(currentTime.format(TIME_FORMAT)))
+        assertThat(epochMillisToTimeString(disruptedLine.endTimestamp), equalTo(currentTime.plusMinutes(30).format(TIME_FORMAT)))
+        assertThat(epochMillisToTimeString(disruptedLine.earliestEndTimestamp), equalTo(currentTime.plusMinutes((30 - 10).toLong()).format(TIME_FORMAT)))
+        assertThat(epochMillisToTimeString(disruptedLine.latestEndTimestamp), equalTo(currentTime.plusMinutes((30 + 10).toLong()).format(TIME_FORMAT)))
+
+        assertThat(disruptedLine.status, equalTo(expectedStatus))
+    }
+
+    private fun epochMillisToTimeString(millis: Long?): String {
+        return LocalDateTime.ofInstant(Instant.ofEpochMilli(millis!!), ZoneOffset.UTC).format(TIME_FORMAT)
+    }
+
+    companion object {
+
+        private val TIME_FORMAT = DateTimeFormatter.ofPattern("HH:mm")
+    }
+
+}
