dd191ae57c052cd7b6f52c3e0ae4fc423be543f2
Callum Stott <cs@pivotal.io>
2016-03-09 10:35:37 +0000

    Set up kotlin main method properly

--- dis-ingest/src/main/kotlin/io/pivotal/dis/ingest/app/Application.kt
+++ dis-ingest/src/main/kotlin/io/pivotal/dis/ingest/app/Application.kt
@@ -1,6 +1,5 @@
 package io.pivotal.dis.ingest.app
 
-import com.amazonaws.services.s3.model.Bucket
 import io.pivotal.dis.ingest.config.ApplicationConfig
 import io.pivotal.dis.ingest.job.Ingester
 import io.pivotal.dis.ingest.store.FileStore
@@ -9,35 +8,32 @@ import io.pivotal.dis.ingest.system.Clock
 import io.pivotal.dis.ingest.system.ClockImpl
 import java.net.URL
 
-object Application {
+fun main(args: Array<String>) {
+    val applicationConfig = ApplicationConfig()
 
-    fun main(args: Array<String>) {
-        val applicationConfig = ApplicationConfig()
+    startIngesting(
+            applicationConfig.tflUrl,
+            applicationConfig.rawFileStore,
+            applicationConfig.digestedFileStore,
+            ClockImpl(),
+            OngoingDisruptionsStore())
+}
 
-        startIngesting(
-                applicationConfig.tflUrl,
-                applicationConfig.rawFileStore,
-                applicationConfig.digestedFileStore,
-                ClockImpl(),
-                OngoingDisruptionsStore())
-    }
+private fun startIngesting(url: URL,
+                           rawFileStore: FileStore,
+                           digestedFileStore: FileStore,
+                           clock: Clock, ongoingDisruptionsStore: OngoingDisruptionsStore) {
 
-    private fun startIngesting(url: URL,
-                               rawFileStore: FileStore,
-                               digestedFileStore: FileStore,
-                               clock: Clock, ongoingDisruptionsStore: OngoingDisruptionsStore) {
+    val ingester = Ingester(url, rawFileStore, digestedFileStore, ongoingDisruptionsStore)
 
-        val ingester = Ingester(url, rawFileStore, digestedFileStore, ongoingDisruptionsStore)
+    while (true) {
+        ingester.ingest(clock)
 
-        while (true) {
-            ingester.ingest(clock)
-
-            try {
-                Thread.sleep(60000)
-            } catch (e: InterruptedException) {
-                e.printStackTrace()
-            }
-
+        try {
+            Thread.sleep(60000)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
         }
+
     }
 }
